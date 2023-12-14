b15bcfc6b9dc057f050382ae5690be3541c33610
vaishnavi_singh <119932034+vaishnavi-23-10@users.noreply.github.com>
2023-12-14 20:48:21 +0530

    Day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  final int days = 31;
+  final String name = "Vaishnavi";
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomePage"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:appmake/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-            child: Center(
-      child: Container(
-        child: Text("Welcome to 30 days of flutter"),
-      ),
-    )));
+      home: Homepage(),
+    );
   }
 }
